4ab26ec4135ea3f79b842e9d72a633c8283c3f47
lostapplesauce <48301604+lostapplesauce@users.noreply.github.com>
2023-11-26 01:59:31 +0000

    End of section 5

---
+++ client-app/src/app/models/activity.ts
@@ -0,0 +1,9 @@
+export interface Activity {
+    id: string  //This is why in ActivityDashboard id is declared as a string instead of an int.
+    title: string
+    date: string
+    description: string
+    category: string
+    city: string
+    venue: string
+  }
